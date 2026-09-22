cc22c3f3b8585bec87473290c2548a11f53e2ef7
mansi Sinha <manshi0832@gmail.com>
2026-09-22 10:18:04 +0530

    Server compenents

--- first/app/page.tsx
+++ first/app/page.tsx
@@ -1,9 +1,15 @@
+"use client"
 import Image from "next/image";
-
-export default function Home() {
+// import fs from "fs/promises"
+import {useState} from "react";
+ export default function Home() {
+const [count, setcount] = useState(0)
+//   let a =fs.readFile(".gitignore")
+//   a.then(e=>console.log(e.toString()))
   return (
-    <div>
-      I am home page
+    <div>I am component
+       {count}<br/>
+      <button onClick={()=>setcount(count+1)}>Click</button>
     </div>
   );
 }
