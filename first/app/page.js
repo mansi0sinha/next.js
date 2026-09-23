d1dab540a6e373adf04c812464b67fcd09358cc4
mansi Sinha <manshi0832@gmail.com>
2026-09-23 08:46:21 +0530

    APIs using next.js

--- first/app/page.js
+++ first/app/page.js
@@ -1,8 +1,32 @@
+"use client"
 import Image from "next/image";
 
 export default function Home() {
+  const handleClick = async () => {
+    let data={
+      name:"mansi",
+      role:"coder"
+    }
+     let a = await fetch("/api/add", {
+      method: "POST",
+      headers: {
+        "Content-type": "application/json",
+      },
+      redirect: "follow",
+      body: JSON.stringify(data),
+    });
+
+    let res = await a.json();
+    console.log(res);
+  };
+
+  
   return (
     <div className="container mx-auto">
+      <div>
+        <h1 className="text-xl font-bold">Next.js Api demo</h1>
+        <button onClick={handleClick}>Click Me</button>
+      </div>
       <Image
         className="mx-auto"
         width={100}
