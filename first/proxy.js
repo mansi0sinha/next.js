ba8de38333d60652230668c866265331947bcc40
mansi Sinha <manshi0832@gmail.com>
2026-09-23 10:13:52 +0530

    Update proxy.js

--- first/proxy.js
+++ first/proxy.js
@@ -1,9 +1,9 @@
 import { NextResponse } from 'next/server'
  
 export function proxy(request) {
-  return NextResponse.redirect(new URL('/home', request.url))
+  return NextResponse.redirect(new URL('/', request.url))
 }
  
 export const config = {
-  matcher: '/about2/:path*',
+  matcher: '/about/:path*',
 }
